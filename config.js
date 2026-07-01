bf07272d5bea177451d52af517e199107418a40a
GhodB <vinegub@gmail.com>
2026-07-01 18:25:26 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 const SUPABASE_URL = "https://jucnkddbngxowycwkmkf.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_PnKqBWS8EBhaYm3kCeDuXw_m7iBdMbn";
+const SUPABASE_ANON_KEY = "sb_publishable_Xz9jbn1PNUAnNYnHPeBErQ_gUX_7frE";
